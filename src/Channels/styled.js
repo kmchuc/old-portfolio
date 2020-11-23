1ebe52bb7a3bc6d0edb9a207af860a8c9e0c13d4
Kimberly Chuc <kimmchuc@gmail.com>
2020-11-23 10:30:34 -1000

    trying to make a loading page animation, not sure if it'll work but lets try it

---
+++ src/Channels/styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const ChannelsContainer = styled.div`
+  display: flex;
+  flex-direction: column; 
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+export const ChannelsTitle = styled.div`
+  display: flex;
+  
+  height: 100px;
+  width: 200px;
+`;
+
+export const ChannelsTitleText = styled.p`
+  font-family
+`;
+
