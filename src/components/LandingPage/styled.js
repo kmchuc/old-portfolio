a92624efd74871f12c6c122c2993aab1f97c4bfb
Kimberly Chuc <kimmchuc@gmail.com>
2021-01-31 16:32:09 -1000

    i have no idea

--- src/components/LandingPage/styled.js
+++ src/components/LandingPage/styled.js
@@ -44,6 +44,7 @@ export const KimzImg = styled.img`
 
     @media screen and (device-width: 414px) and (device-height : 667px) and (-webkit-device-pixel-ratio : 3){
         height: 300px;
+        width: 300px;
         object-fit: cover;
         object-position: center;
         
