59bb06a40f96bec6187b34daf62e7572a52f4fef
Kimberly Chuc <kimmchuc@gmail.com>
2021-01-31 16:11:57 -1000

    changed to block

--- src/components/LandingPage/styled.js
+++ src/components/LandingPage/styled.js
@@ -43,8 +43,8 @@ export const KimzImg = styled.img`
     height: auto;
 
     @media screen and (device-width: 414px) and (device-height : 667px) and (-webkit-device-pixel-ratio : 3){
-        height: auto;
-        width: 100%;
+        display: block;
+        
     }
 `;
 
